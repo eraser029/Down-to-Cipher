b2f737db96c4e8bf852d4f73fc60b63d0c250dc0
Ashish Sharma <ashisharma@outlook.com>
2014-07-07 03:31:40 +0530

    Committing the full application.
    This is working application. More development will depend on how much I
    use it and if I would need any changes to it.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,16 @@
 /**
  *
- *  Web Starter Kit
- *  Copyright 2014 Google Inc. All rights reserved.
+ *  Down to Cipher
+ *  Copyright 2014. All rights reserved.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  Licensed under the Apache License, Version 2.0 (the 'License');
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  distributed under the License is distributed on an 'AS IS' BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License
@@ -43,3 +43,113 @@
         }
     });
 })();
+
+// Angular JS related
+/*global angular */
+angular.module('CounterModule', [])
+    .filter('filterOutHistoryItemColorClass', function () {
+        'use strict';
+        
+        return function (input) {
+            var out;
+            switch (input) {
+                case 'CIPHERED':
+                    out = 'color--yellow';
+                    break;
+                case 'REDUCED':
+                    out = 'color--green';
+                    break;
+                case 'RESET':
+                    out = 'color--red';
+                    break;
+                default:
+                    out = 'color--blue';
+            }
+            return out;
+        };
+    })
+    .controller('CounterCtrl', ['$scope',
+        function ($scope) {
+            'use strict';
+
+            $scope.counter = 10;
+            $scope.title = 'Down to Cipher';
+            $scope.history = [
+                {
+                    'type': 'START',
+                    'value': '*Down to Cipher* is loaded. Counter set to 10.'
+                },];
+            
+            
+            $scope.appendToHistory = function (type, text) {
+                var item = {
+                    'type': type,
+                    'value': text
+                };
+                $scope.history.unshift(item);
+            };
+
+            $scope.reduceTheCounter = function () {
+                var reduceCounterVal = 0;
+                try {
+                    reduceCounterVal = parseInt($scope.reduceValue);
+                    if (isNaN(reduceCounterVal)) {
+                        reduceCounterVal = 0;
+                    }
+                } catch (err) {
+                    console.log('Could not reduce value, input is not proper integer');
+                    reduceCounterVal = 0;
+                }
+                $scope.reduceValue = reduceCounterVal;
+                $scope.counter -= reduceCounterVal;
+                
+                //Make a history entry
+                $scope.appendToHistory('REDUCED', 'Counter reduced by *' + reduceCounterVal + '* on ' + new Date() + '.');
+                
+                if ($scope.counter <= 0) {
+                    $scope.counter = 0;
+                    $scope.celebrate();
+                }
+            };
+
+            $scope.celebrate = function () {
+                //Make a history entry
+                $scope.appendToHistory('CIPHERED', 'YAAAY! Counter successfully ciphered on ' + new Date() + '.');
+            };
+
+            $scope.changeTheTitle = function () {
+                var TITLE_LENGTH_LIMIT = 36;
+                if ($scope.titleValue.length > TITLE_LENGTH_LIMIT) {
+                    $scope.titleValue = $scope.titleValue.substring(0, TITLE_LENGTH_LIMIT);
+                }
+                //Make a history entry
+                $scope.appendToHistory('TITLE', 'Title changed to *' + $scope.titleValue + '* on ' + new Date() + '.');
+                $scope.title = $scope.titleValue;
+                document.title = $scope.titleValue +' : Down to Cipher';
+            };
+
+            $scope.resetTheCounter = function () {
+                var COUNTER_SIZE_LIMIT = 1000000;
+                var resetCounterVal = 0;
+                try {
+                    resetCounterVal = parseInt($scope.resetValue);
+                    if (isNaN(resetCounterVal)) {
+                        resetCounterVal = 0;
+                    }
+                    if (resetCounterVal > COUNTER_SIZE_LIMIT) {
+                        resetCounterVal = COUNTER_SIZE_LIMIT;
+                    }
+                    if (resetCounterVal < 0) {
+                        resetCounterVal = 0;
+                    }
+                } catch (err) {
+                    console.log('Could not reset to input , input is not proper integer');
+                    resetCounterVal = 0;
+                }
+                //Make a history entry
+                $scope.appendToHistory('RESET', 'Counter reset to *' + resetCounterVal + '* on ' + new Date() + '.');
+                $scope.resetValue = resetCounterVal;
+                $scope.counter = resetCounterVal;
+            };
+
+  }]);
